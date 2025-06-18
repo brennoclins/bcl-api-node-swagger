2fa7d5e6a1edf7cb3d1da23b9471b84e1ea38050
Brenno <dev@bcl-st.com.br>
2025-06-18 09:19:43 -0300

    chore: instala o swagge [ @fastify/swagger, @fastify/swagger-ui ]

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
-import  fastifly  from "fastify"
-import  fastiflyCors   from "@fastify/cors"
+import  { fastify } from "fastify"
+import  { fastifyCors }  from "@fastify/cors"
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
+import {fastifySwagger} from "@fastify/swagger"
+import fastifySwaggerUi from "@fastify/swagger-ui"
 
 
-const app = fastifly()
+const app = fastify()
 
 // Faz a validação dos dados
 app.setValidatorCompiler(validatorCompiler)
 // Faz a serialização dos dados de resposta do backend
 app.setSerializerCompiler(serializerCompiler)
 
-app.register( fastiflyCors, { origin: '*'})
+app.register( fastifyCors, { origin: '*'})
+
+app.register(fastifySwagger, {
+    openapi:{
+        info: {
+            title: 'Typed API',
+            version: '1.0.0'
+        }
+    }
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs'
+})
+
+// rotas
+app.get('/', () => {
+    return 'Olá Mundo!'
+})
 
 app.listen({ port: 3333 }).then(() => {
 console.log(`Servidor rodando em http://localhost:3333`);
